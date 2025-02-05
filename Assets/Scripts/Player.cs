b47b955ce9bb40a50463f6bb0203fc87569faa28
Zzalbs <kcjlazer@naver.com>
2025-02-05 15:27:21 +0900

    좌우이동, 점프 추가

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,15 +5,30 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed;
+    private float moveDir;
+    private Rigidbody2D rb;
+    public float jumpPower;
 
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();    
     }
 
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.Translate(Vector2.left * speed * Time.deltaTime);    
+        if(Input.GetKey(KeyCode.A))
+        {
+            gameObject.transform.Translate(Vector2.left * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            gameObject.transform.Translate(Vector2.right * speed * Time.deltaTime);
+        }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector2.up * jumpPower,ForceMode2D.Impulse);
+        }
     }
+
 }
